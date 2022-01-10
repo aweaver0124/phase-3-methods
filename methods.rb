d29242a2c0bf9b5bfecb5148e5b0e039d77ad8d4
Alexandra Weaver <aweaver0124@gmail.com>
2022-01-10 11:59:34 -0600

    commit tree 78824d98054202cec9a92c1014186190b4455166

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+# method 1
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# method 2
+def greet name
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+# method 3
+def greet_with_default (name = 'programmer')
+    puts "Hello, #{name}!"
+end
+greet_with_default("Sunny")
+
+# method 4
+def add num1, num2
+    return num1 + num2
+end
+add(1, 2)
+
+# method 5
+def halve num1
+    if num1.class != Integer
+        return nil
+    end
+    return num1 / 2
+end
+halve(4)
+halve("hello")
